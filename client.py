1290ebba94b25f425b36bdc91f7b0cb54384d626
David <jd.monsalve@uniandes.edu.co>
2020-10-27 22:28:19 -0500

    archivos de streaming de video, con la herramienta VLC

---
+++ client.py
@@ -0,0 +1,44 @@
+import socket
+import os
+import time
+import numpy as np
+
+os.system('sudo ntpdate 0.es.pool.ntp.org')
+UDP_IP = '127.0.0.1'
+UDP_PORT = 5681
+UDP_PORT2 = 5682
+print ("Preparado para recibir datos")
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock2 = socket.socket (socket.AF_INET, socket.SOCK_DGRAM)
+socketPlayer = socket.socket (socket.AF_INET, socket.SOCK_DGRAM)
+socket. socket (socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((UDP_IP, UDP_PORT))
+socketPlayer.bind(('', 8081))
+socketPlayer.listen(1)
+print ('Esperando')
+conn, addr = socketPlayer.accept()
+print ('Conexion establecida con VLC')
+sock2.sendto("hola", (UDP_IP, UDP_PORT2))
+
+lista = [None] * 128
+bandera = 0
+total = 0
+data, addr = sock.recvfrom(2048)
+elementos = data.split("----")
+conn.sendall(elementos[2])
+
+while True:
+    data, addr = sock.recvfrom(2048)
+    elementos = data.split("----")
+    hora = elementos[1]
+    print(time. time() - float(hora))
+    total+=1
+
+    if bandera==1:
+        conn.sendall(lista[total%128])
+
+    lista[((elementos[0]))%128] = elementos[2]
+    if total == 127:
+        bandera = 1
+
+sock.close()
---
+++ server.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 26 11:16:00 2020
+
+@author: Juanda2420
+"""
+
+import socket
+import sys
+import time
+import os
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5681 
+UDP_PORT2 = 5682
+os.system("sudo ntpdate 0.es.pool.ntp.org")
+rec = 0
+def autoIncremenet():
+    global rec
+    pStart = 1
+    pInterval = 1
+    if(rec == 0):
+        rec = pStart
+    else:
+        rec = rec + pInterval
+    return str(rec).zfill(7)
+
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error:
+    print("Fallo al conectar al socket del player")
+    sys.exit()
+    
+print("Socket creado!!!!")
+
+hostSource = ""
+portSource = 8080
+channelSource = ""
+
+try:
+    remote_ip = socket.gethostbyname(hostSource)
+except socket.gaierror():
+    print("No es posible conectarse al host")
+    sys.exit()
+    
+s.connect((remote_ip, portSource))
+sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock2.bind((UDP_IP, UDP_PORT2))
+print("Socket conectado al reproductor VCL!!!!")
+data, addr = sock2.recvfrom(1024)
+
+message = "GET/" + channelSource + "HTTP/1.1\r\n\r\n"
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.sendall(message)
+
+print("....Enviando datos por UDP a la dirección: " + UDP_IP)
+
+while True:
+    try:
+        reply = s.recv(1024)
+        sock.sendto(autoIncremenet() + "-----"+ str(time.time())+ "------"+reply, (UDP_IP, UDP_PORT))
+    except socket.error:
+        print("Mensaje no se ha podido enviar correctamente")
+        sys.exit()
+        
+
+s.close()
+sock.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
